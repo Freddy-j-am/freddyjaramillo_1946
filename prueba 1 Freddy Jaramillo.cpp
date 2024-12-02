44bc9edb4014e73ac2e752dae627c081c5830c40
freddy jaramillo <freddy.jaramillo506@gmail.com>
2024-12-02 10:09:18 -0500

    Foto2/12/2024

---
+++ prueba 1 Freddy Jaramillo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n1, n2, n3, aux;
+main(){
+    cout<< "ingrese el valor de n1 = \t";
+    cin >> n1;
+    cout<< "ingrese el valor de n2 = \t";
+    cin >> n2;
+    cout << "ingrese el valor de n3 = \t";
+    cin >> n3;
+    aux = n1;
+    cout << "el valor de aux guardara = "<<n1<<endl;
+    cout << "el valor de n1 tomara = " <<n3<<endl;
+    cout << "el valor de n2 tomara = "<<aux<<endl;
+    cout << "el valor de n3 tomara = "<<n2<<endl;
+
+}
