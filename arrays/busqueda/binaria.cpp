c1df2e9d3882f29d4f7f945fb729c2fb340dac05
freddy jaramillo <freddy.jaramillo506@gmail.com>
2025-02-07 09:19:39 -0500

    Librerias

--- arrays/busqueda/binaria.cpp
+++ arrays/busqueda/binaria.cpp
@@ -1,55 +1,49 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "../ordenamiento/seleccion.cpp"
+#include "../../librerias/arrays.h"
 using namespace std;
-void llenaDatos(int v[], int n)
-{
-srand(time(NULL));
-    for (int i= 0;i<n;i++){
-        v[i]=rand()%20+1;
-        //cin >> v[i];
-        }    
-}
-void ordenarArray(int v[], int n)
-{
-    int aux;
-    bool cambios=true;
-    for(int i=0;i<n-1 && cambios;i++){
-        cambios = false;
-        for( int j= i+1;j<n;j++){
-            if (v[i]>v[j]){
-                aux = v[i];
-                v[i]= v[j];
-                v[j]= aux;
-                cambios = true;
-            }
-        }
-    }
-
-}
-
-void verDatos(int v[], int n)
-{
-for(int i=0; i<n;i++){
-        cout << v[i]<<" ";
-    }
-}
+using namespace vectorn;
 bool isBusquedaBinaria(int v[], int n, int elemento)
 {
-    int Iabajo, Iarriba, Icentro;
-    for( Iabajo = 0;  ){
+    int Iabajo = 0, Iarriba = n - 1, Icentro;
 
+    while (Iabajo <= Iarriba)
+    {
+        Icentro = (Iabajo + Iarriba) / 2;
+
+        if (v[Icentro] == elemento)
+        {
+            return true; // Se encontró el elemento
+        }
+        else if (v[Icentro] < elemento)
+        {
+            Iabajo = Icentro + 1; // Buscar en la mitad derecha
+        }
+        else
+        {
+            Iarriba = Icentro - 1; // Buscar en la mitad izquierda
+        }
     }
+    return false; // No se encontró el elemento
 }
+
 main()
 {
-    int ne, dato;
+    int ne, dato, ri , rs;
     cout << "Nro de Elementos del Array: ";
     cin >> ne;
     int vector[ne];
-    llenaDatos(vector, ne);
-    cout<< "Ingrese el dato a buscar: ";
-    (isBusquedaBinaria(vector, ne,dato))?cout<<"Dato Encontrado ": cout<< "Dato no encontrado" ;
+    llenarVector(vector, ne, ri, rs);
+    cout << "Vector original: ";
+    verDatos(vector,ne);
+    cout<<endl;
+    ordenarBurbujav3(vector, ne);
+    cout<< "Vector ordenado: ";
+    verDatos(vector, ne);
+    cout<<endl;
+    cout << "Ingrese el dato a buscar: ";
     cin >> dato;
+
+    (isBusquedaBinaria(vector, ne, dato)) ? cout << "Dato Encontrado" : cout << "Dato no encontrado";
 }
