361f94a0e255959de170d00fecd019f513799c9b
freddy jaramillo <freddy.jaramillo506@gmail.com>
2025-02-19 10:39:09 -0500

    clase registroarray 19/02/2025

--- registros/actividad1registro.cpp
+++ registros/actividad1registro.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
-struct rAutomoviles{
+
+struct rAutomoviles {
     int codigoA;
     string marca;
     string modelo;
     int anio;
     string placa;
-}r1;
-struct clienteVideoclub{
+} r1 = {1234, "Toyota", "Corolla", 2020, "XYZ-123"};
+
+struct clienteVideoclub {
     string nombre;
     string direccion;
     int telefono;
     int adeuda;
     int codigocl;
-}c1;
-struct licenciaManejo{
+} c1 = {"Juan Perez", "Av. Principal 123", 987654321, 500, 5678};
+
+struct licenciaManejo {
     string nombres;
     string apellidoPaterno;
     string apellidoMaterno;
@@ -27,11 +30,17 @@ struct licenciaManejo{
     string donaOrganos;
     string direccion;
     int cedula;
-}l1;
-struct tarjetaCredito{
+} l1 = {"Carlos", "Gomez", "Fernandez", "01/01/2022", "01/01/2032", "Mexicana", "O+", "A", "Ninguna", "Si", "Calle Falsa 123", 12345678};
+
+struct tarjetaCredito {
     string nombre;
     string fechaVencimiento;
     int numeroTarjeta;
     int saldo;
     int limite;
-}t1;
+} t1 = {"Luis Martinez", "12/2027", 987654321, 15000, 50000};
+
+int main() {
+    cout << "Datos inicializados correctamente." << endl;
+    return 0;
+}
